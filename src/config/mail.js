f86d909a9b69ea8df1630112a3def86e7cf98946
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-01 23:26:44 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '08fb0cdb36a9a0',
+    pass: 'f4e50f5396d5d5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
